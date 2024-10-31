9f0be705070ed99f7bf75304c24618ffc3204003
Hassan2628 <Hassan.Warsame@laetottenham.ac.uk>
2024-10-31 13:59:35 +0000

    Pending changes exported from your codespace

--- main.py
+++ main.py
@@ -1,32 +1,63 @@
+
+import os
 cat_attributes = {
-    "intelligence": 0,
-    "energy": 0,
-    "weight": 0,
+    "intelligence": 50,
+    "energy": 100,
+    "weight": 50,
     # change the inital values above
 }
 
 print("Welcome to my cat game!")
 
 # Take the user inputs for name and colour:
-name = input("Enter name:")
-# ...
+name = input("Enter the name of your cat:")
+colour = input("Enter the colour of your cat:")
 
 # start the while loop
 while True:
     # Finish the string below
-    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats")
+    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. Feed your cat 4. Put your cat to sleep 5. show stats ")
 
     if option == '1':
         # change the cat's attributes here
-        pass
+       os.system("clear")
+       cat_attributes["energy"] += -10
+       cat_attributes["intelligence"] += 5
+       cat_attributes["weight"] += -5
+       print(cat_attributes)
     elif option == '2':
-        pass
+       os.system("clear")
+       cat_attributes["energy"] += -20
+       cat_attributes["intelligence"] += 10
+       cat_attributes["weight"] += -10
+       print(cat_attributes)
+    elif option == '3':
+       os.system("clear")
+       cat_attributes["energy"] += 10
+       cat_attributes["intelligence"] += -7
+       cat_attributes["weight"] += 10
+       print(cat_attributes) 
+    elif option == '4':
+       os.system("clear")
+       cat_attributes["energy"] += 15
+       cat_attributes["intelligence"] += -5
+       cat_attributes["weight"] += 5
+       print(cat_attributes)  
+    elif option == '5': 
+        os.system("clear")    
+        print(cat_attributes)
+        
     # elif ...
     else:
-        pass
+       os.system("clear")
+       print("Please pick an option.")
+
+        
 
     # finish off the if statements below
-    if cat_attributes['energy'] < 0:
-        pass
+    if cat_attributes['energy'] < 5:
+        os.system("clear")
+        print("Your cat has died and the game has ended, better luck next time.")
+        print(cat_attributes)
+        break
     # elif ...
-    
